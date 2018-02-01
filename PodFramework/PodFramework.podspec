207689320e761947e613171d1a77bc3ce8fb6a22
demo <demo@dipa-mac.local>
2018-02-02 01:14:45 +0530

    Added all files in Repo

---
+++ PodFramework/PodFramework.podspec
@@ -0,0 +1,16 @@
+
+Pod::Spec.new do |s|
+
+  s.name         = "PodFramework"
+  s.version      = "1.0.0"
+  s.summary      = "This is sample podframework"
+  s.description  = "This is sample podframework, to do practice"
+  s.homepage     = "https://github.com/DipaG/PodFramework"
+  s.license      = "MIT"
+  s.author       = { "Dipa" => "" }
+  s.platform     = :ios, "11.0"
+  s.source       = { :git => "https://github.com/DipaG/PodFramework.git", :tag => "1.0.0" }
+  s.source_files  = "PodFramework/**/*"
+
+
+end
---
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by demo on 02/02/18.
+//
+
+import UIKit
+import PodFramework
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(serv)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
