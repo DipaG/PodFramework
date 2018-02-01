2b23b31d903c6f3c29fdbd8b53999a9fa78d9548
demo <demo@dipa-mac.local>
2018-02-01 23:55:52 +0530

    Finished local framework

---
+++ PodFramework/PodFramework/PodFramework.h
@@ -0,0 +1,19 @@
+//
+//  PodFramework.h
+//  PodFramework
+//
+//  Created by demo on 01/02/18.
+//  Copyright © 2018 Dipa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodFramework.
+FOUNDATION_EXPORT double PodFrameworkVersionNumber;
+
+//! Project version string for PodFramework.
+FOUNDATION_EXPORT const unsigned char PodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFramework/PublicHeader.h>
+
+
---
+++ PodFramework/PodFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  PodFramework
+//
+//  Created by demo on 01/02/18.
+//  Copyright © 2018 Dipa. All rights reserved.
+//
+
+import Foundation
+class Service{
+    private init(){}
+    public static func doSomething(){
+        print("Hello")
+    }
+    
+}
